8586ea544e92261aea1004c9b2c0029a8e62bcbd
Cherry <chrryy@gmail.com>
2015-08-28 03:43:33 +0200

    xadded avoidance module

--- sw/airborne/modules/cherrytest/test1.c
+++ sw/airborne/modules/cherrytest/test1.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014
+ * Copyright (C) 2015
  *
  * This file is part of paparazzi.
  *
@@ -20,20 +20,45 @@
  *
  */
 
-/** @file follow.c
- *  @brief Follow a certain AC ID.
- * Only for rotorcraft firmware.
- */
-
 #include "cherrytest/test1.h"
 #include "state.h"
 
 
+float d_oi;
+float d_vo;
+float r_vo;
+float rpz;
+float theta;
+
+double DD_oi[2][1];
+double A[2][1];
+float C;
+float azimuthangle;
+
+
 void avoidance_alg(void)
 {
+
 	printf("hey %f\n", stateCalcPositionEcef_i);
 	printf("hey %f\n", stateCalcPositionNed_i);
 	printf("hey %f\n", stateCalcPositionEnu_i);
+
+	d_oi = 2;
+	d_vo = (pow(d_oi, 2) - pow(rpz, 2)) / d_oi;
+	r_vo = rpz * (sqrt  (pow(d_oi,2) - pow(rpz,2)   )   )/d_oi;
+	theta = atan(r_vo/d_vo);
+	
+	DD_oi[1][1] = cos(azimuthangle) * d_vo;
+	DD_oi[2][1] = sin(azimuthangle) * d_vo;
+
+
+	double B(double A[], double DD_oi[], int n)	
+	{
+	double result = 0.0;
+	for (int i = 0; i < n; i++)
+		result += A[i]*DD_oi[i];
+	return result;
+	}
 
 
 }
